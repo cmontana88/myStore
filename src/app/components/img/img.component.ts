4daf2205f320683187de578b3aac8297041740e5
cmontana <cmontana@skandia.com.co>
2021-11-20 16:55:08 -0500

    final del proyecto aplicando componentes, pipes, services, directives

---
+++ src/app/components/img/img.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-img',
+  templateUrl: './img.component.html',
+  styleUrls: ['./img.component.scss']
+})
+export class ImgComponent implements OnInit, OnChanges {
+
+  @Input() img: string = '';
+  @Output() loaded = new EventEmitter();
+  imageDefault:string = './assets/images/default.png';
+
+  constructor() {
+    console.log('constructor', 'imgValue =>', this.img);
+  }
+
+  ngOnChanges(){
+    console.log('ngOnChanges', 'imgValue =>', this.img);
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  imgError(){
+    this.img = this.imageDefault;
+  }
+
+  imgLoaded(){
+    console.log('Cargado con exito');
+    this.loaded.emit(this.img);
+  }
+
+}
