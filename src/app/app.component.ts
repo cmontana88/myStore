04e95ac112f55c87707402e1982a4835e1eab22f
cmontana <cmontana@skandia.com.co>
2021-11-20 16:56:03 -0500

    ultimo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myStore';
+  imgParent = '';
+
+  onLoaded(img: string){
+    console.log('log padre ' + img);
+  }
 }
---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { ProductsService } from 'src/app/services/products.service';
+import { StoreService } from 'src/app/services/store.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+
+  total = 0;
+  myShoppingCart: Product[] = [];
+
+  products: Product[] = [];
+  today = new Date();
+  date = new Date(2021, 1, 21);
+
+  constructor(private storeService: StoreService, private productService: ProductsService) {
+    this.myShoppingCart = this.storeService.getShoppingCart();
+  }
+
+  ngOnInit(): void {
+    this.productService.getAllPruducts().subscribe(data => {
+      this.products = data;
+    });
+  }
+
+  onAddToShoppingCart(product: Product) {
+    this.storeService.addProduct(product);
+    this.total = this.storeService.getTotal();
+  }
+
+}
